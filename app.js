891ceb8d3a23b0e689bff740b9dae5d88a2c389d
abhinaysingh27 <abhinay.chiku1234@gmail.com>
2025-09-02 01:49:59 +0530

    done

--- app.js
+++ app.js
@@ -8,21 +8,26 @@ import { dbConnection } from "./database/dbConnection.js";
 const app = express();
 dotenv.config({ path: "./config.env" });
 
+// CORS setup
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
+    origin: process.env.FRONTEND_URL, // frontend URL as string
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/reservation", reservationRouter);
-app.get("/", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "HELLO WORLD AGAIN"
-})})
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "HELLO WORLD AGAIN"
+  });
+});
 
 dbConnection();
 
