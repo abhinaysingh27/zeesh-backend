98737b60ce7443ccb74a3d4e70a25bec92ff7f52
abhinaysingh27 <abhinay.chiku1234@gmail.com>
2025-09-02 02:00:46 +0530

    done

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -3,6 +3,29 @@ import send_reservation from "../controller/reservation.js";
 
 const router = express.Router();
 
-router.post("/send", send_reservation);
+// Handle preflight OPTIONS request
+router.options("/send", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://zeesh-frontend.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.status(200).end();
+});
+
+// POST route with CORS headers
+router.post("/send", async (req, res, next) => {
+  try {
+    // Set CORS headers explicitly
+    res.setHeader("Access-Control-Allow-Origin", "https://zeesh-frontend.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    // Call your controller logic
+    await send_reservation(req, res, next);
+
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
+
